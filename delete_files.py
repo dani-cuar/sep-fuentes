277307e0a42363028cdf60ebdaa11eca8db395b7
DanielaC <daniela.cuartasm@udea.edu.co>
2024-01-12 18:54:24 -0500

    comentarios actualizados

--- delete_files.py
+++ delete_files.py
@@ -1,21 +1,28 @@
 import os
 
 def synchronize_folders(folder1, folder2, extension1, extension2):
+    # Get a list of files in folder1 with the specified extension1, removing the extension
     files1 = [file.split('.')[0] for file in os.listdir(folder1) if file.endswith(extension1)]
+    
+    # Get a list of files in folder2 with the specified extension2, removing the extension
     files2 = [file.split('.')[0] for file in os.listdir(folder2) if file.endswith(extension2)]
 
+    # Find the common files by creating a set of unique file names from folder1
     common_files = set(files1)
 
+    # Iterate through files in folder2
     for file in files2:
+        # Check if the file is not in the common_files set
         if file not in common_files:
+            # Construct the full path of the file in folder2 with its extension and remove it
             file_path = os.path.join(folder2, file)
             os.remove(file_path + ".wav")
 
-# Rutas de las carpetas y extensiones
-folder_path1 = "C:/Users/Daniela Cuartas/Documents/UdeA/Proyecto de grado/database_v3/espectrogramas/male_male"
-folder_path2 = "C:/Users/Daniela Cuartas/Documents/UdeA/Proyecto de grado/database_v3/audios/male_male"
+# Paths of the folders and file extensions
+folder_path1 = "C:/Users/Daniela Cuartas/Documents/UdeA/Proyecto de grado/database_v3/espectrogramas/female_female"
+folder_path2 = "C:/Users/Daniela Cuartas/Documents/UdeA/Proyecto de grado/database_v3/audios/female_female"
 extension1 = ".png"
 extension2 = ".wav"
 
-# Sincronizar las carpetas eliminando archivos que no tienen correspondencia
+# Synchronize the folders by removing files in folder2 that do not have a corresponding file in folder1
 synchronize_folders(folder_path1, folder_path2, extension1, extension2)
